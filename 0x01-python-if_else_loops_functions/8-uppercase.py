10534de1642e10eab5674877cab7701558596923
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-11-29 00:47:47 -0800

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        if 'a' <= char <= 'z':
+            uppercase_char = chr(ord(char) - ord('a') + ord('A'))
+            print("{}".format(uppercase_char), end='')
+        else:
+            print("{}".format(char), end='')
+
+    print()
+
