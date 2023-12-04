1595f3258cffef57d2a429b719d70570eabd2b7c
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-12-04 00:33:15 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-# Assigning values to variables a and b
-a = 1
-b = 2
-
 # Importing the add function from add_0.py
 from add_0 import add
+if __name__ == "__main__":
+
+# Assigning values to variables a and b
+    a = 1
+    b = 2
 
 # Using string.format to print the result
 print("{} + {} = {}".format(a, b, add(a, b)))
-
