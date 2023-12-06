9509306488acfecfdd152b3a122da07755d21f02
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-12-05 17:30:01 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,8 @@ if __name__ == "__main__":
 
     argv = sys.argv[1:]
     num_args = len(argv)
-    print("{} argument{}".format(num_args, "s" if num_args != 1 else "",), end="")
+    print("{} argument{}".format(
+        num_args, "s" if num_args != 1 else "",), end="")
     print(":" if num_args > 0 else ".")
 
     for i, arg in enumerate(argv, start=1):
