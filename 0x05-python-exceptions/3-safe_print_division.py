a0cfe367ad6465ffe19c813420b9a150c6321da0
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-12-19 02:19:23 -0800

    3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/bin/usr/python3
+def safe_print_division(a, b):
+
+    try:
+        result = (a/b)
+    except:
+        ZeroDivisionError
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
+
