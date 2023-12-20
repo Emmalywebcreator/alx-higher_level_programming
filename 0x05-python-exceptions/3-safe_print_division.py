f7b70e521ae4c9ebf10914f9b45d23d6ee94461f
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2023-12-20 13:25:20 -0800

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,4 +1,4 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 def safe_print_division(a, b):
 
     try:
@@ -9,4 +9,3 @@ def safe_print_division(a, b):
     finally:
         print("Inside result: {}".format(result))
         return result
-
