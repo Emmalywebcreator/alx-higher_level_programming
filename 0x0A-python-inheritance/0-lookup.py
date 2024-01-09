16c7d76c85003b4a650bd387f696b1f23f436968
Ohwoka Emmanuel <emmyprime2015@gmail.com>
2024-01-09 13:04:02 -0800

    lookup function updated 2

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,4 +2,4 @@
 """Defines and object attributes lookup function."""
 def lookup(obj):
     """Return a list of available attributes and methods of an object."""
-    return dir(obj)
+    return (dir(obj))
