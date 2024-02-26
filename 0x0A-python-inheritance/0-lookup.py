6d04f9134732055bf5c52ae96b77613c55381833
Emmalywebcreator <120689970+Emmalywebcreator@users.noreply.github.com>
2024-02-26 10:58:36 +0100

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Defines and object attributes lookup function."""
-
-
-def lookup(obj):
-    """Return a list of available attributes of an object."""
-    return (dir(obj))
--- 0x0A-python-inheritance/README.md
+++
@@ -1,1 +0,0 @@
-python inherentance by Emmalywebcreator
