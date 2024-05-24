462cf2d6857236b45300b8a12d1246c501d1304e
Emmanuel Ohwoka <emmpyprime2015@gmail.com>
2024-05-23 18:06:55 -0700

    move to utf8mp4

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,6 @@
 -- Convert the database hbtn_0c_0 to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
--- Use the converted database
-USE hbtn_0c_0;
-
 -- Convert the table first_table to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
