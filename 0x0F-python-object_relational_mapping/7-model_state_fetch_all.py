44089001b96775db95756c85c3aebb96949a0101
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-06-27 20:58:32 +0100

    fetch all state

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,7 +7,7 @@ from model_state import Base, State
 
 
 def list_state_obj():
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
