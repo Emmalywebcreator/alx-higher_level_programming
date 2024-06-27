60a97da40b8cdea4803fef039055c925f752d56d
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-06-27 21:41:29 +0100

    session query updated

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -2,18 +2,19 @@
 """List all State objects containing `a` from db"""
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, sessionmaker
 from model_state import Base, State
 
 
 def list_a_state_obj():
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    session = Session(engine)
+    Session = sessionmaker(bind=engine)
 
+    session = Session()
     rows = session.query(State).all()
 
     for i in rows:
@@ -21,6 +22,7 @@ def list_a_state_obj():
             print("{}: {}".format(i.__dict__['id'], i.__dict__['name']))
 
     session.close()
+
 
 if __name__ == "__main__":
     list_a_state_obj()
