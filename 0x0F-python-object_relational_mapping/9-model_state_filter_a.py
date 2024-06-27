8a5b915f9e68b9947b4df0bb7292f10d3a639f7f
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-06-27 21:53:09 +0100

    session query updated

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-"""
-lists all State objects that contain the letter a from
-the database hbtn_0e_6_usa
-"""
+"""List all State objects containing `a` from db"""
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+
+def list_a_state_obj():
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost3306/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    rows = session.query(State).all()
+
+    for i in rows:
+        if 'a' in i.__dict__['name']:
+            print("{}: {}".format(i.__dict__['id'], i.__dict__['name']))
+
+    session.close()
 
 
 if __name__ == "__main__":
-    """lists all State objects that contain the
-    letter a from the database hbtn_0e_6_usa
-    """
-    engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}\
-    @localhost:3306/{sys.argv[3]}', pool_pre_ping=True)
-
-    Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    for state in session.query(State) \
-                        .filter(State.name.ilike('%a%')) \
-                        .order_by(State.id.asc()):
-        print(f"{state.id}: {state.name}")
+    list_a_state_obj()
