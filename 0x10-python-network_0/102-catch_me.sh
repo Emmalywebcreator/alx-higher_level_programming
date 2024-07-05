88e1d6961bd1a20e61d23a00f9b8d7b09aa6dcab
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-07-04 19:46:20 -0700

    Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that makes a request to 0.0.0.0:5000/catch_me to get the server to respond with "You got me!" in the body of the response
-curl -s -X PUT -d "user_id=98" "http://0.0.0.0:5000/catch_me"
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
