25684ab73c77524a5d45a22c7189fe1386f51ced
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-07-04 17:02:43 -0700

    display body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
-curl -sL "$1" | wc -c
+curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
