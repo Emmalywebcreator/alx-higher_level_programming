24b7e760bb4887294b52480cda642384eec92a95
Ohwoka Emmanule <emmyprime2015@gmail.com>
2024-07-06 21:46:32 -0700

    Response header value #0

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Retrieve and display the X-Request-Id header value from a URL"""
+
+import urllib.request
+import sys
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python get_request_id.py <URL>")
+        return
+    
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            request_id = response.getheader('X-Request-Id')
+            if request_id:
+                print(f"{request_id}")
+            else:
+                print("X-Request-Id header not found in the response.")
+    except Exception as e:
+        print(f"Error fetching URL: {e}")
+
+
+if __name__ == "__main__":
+    main()
