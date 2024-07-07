7b024c5fcfdb2266a45f3bb2165cf4a96d38fe24
Ohwoka Emmanule <emmyprime2015@gmail.com>
2024-07-07 09:36:38 -0700

    POST an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL and an email address,
+sends a POST request to the passed
+URL with the email as a parameter, and finally
+displays the body of the response.
+"""
+
+
+import sys
+import requests
+
+
+def main():
+    """
+    Sends a POST request to a given URL with a given email.
+    """
+
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
+    print(r.text)
+
+
+if __name__ == "__main__":
+    main()
