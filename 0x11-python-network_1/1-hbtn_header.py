fd2a6828676d16f072909e77a5f42298e836987f
Ohwoka Emmanule <emmyprime2015@gmail.com>
2024-07-06 21:59:37 -0700

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-"""Retrieve and display the X-Request-Id header value from a URL"""
+"""
+Retrieve and display the X-Request-Id header value from a URL.
+
+This script takes a URL as input, sends a request to the URL using urllib,
+and prints the value of the X-Request-Id header found in the response.
+"""
+
 
 import urllib.request
 import sys
 
-
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python get_request_id.py <URL>")
-        return
     url = sys.argv[1]
 
     try:
         with urllib.request.urlopen(url) as response:
-            request_id = response.getheader('X-Request-Id')
-            if request_id:
-                print(f"{request_id}")
+            x_request_id = response.getheader('X-Request-Id')
+            if x_request_id:
+                print(f"{x_request_id}")
             else:
                 print("X-Request-Id header not found in the response.")
     except Exception as e:
