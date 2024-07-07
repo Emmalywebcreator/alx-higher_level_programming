edb02591acffbb2a1e3b8525f3e56c6ca45ea253
Ohwoka Emmanule <emmyprime2015@gmail.com>
2024-07-06 20:33:03 -0700

    Whats my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,20 +1,4 @@
 #!/usr/bin/env python3
-"""
-Fetches the status from https://alx-intranet.hbtn.io/status and displays information about the response.
-
-Usage:
-    ./0-hbtn_status.py | cat -e
-
-This script fetches the status from a URL using urllib and displays:
-- The type of the response content.
-- The raw byte content of the response.
-- The UTF-8 decoded content of the response.
-
-It demonstrates basic usage of urllib.request.urlopen() and handling of HTTP responses.
-
-Author:
-    Your Name
-"""
 
 import urllib.request
 
