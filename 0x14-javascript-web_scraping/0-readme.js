797eddcb269db74d40829658356c9895cf36859c
Emmanuel Ohwoka <emmyprime2015@gmail.com>
2024-07-31 05:23:13 -0700

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,8 @@
 const fs = require('fs');
 
 if (process.argv.length > 2){
-  const filePath = process.argv[2];
 
-  fs.readFile(filePath, 'utf8', (data, err) => {
+  fs.readFile(process.argv[2], 'utf8', (data, err) => {
   if (err) {
     console.error(err);
   } else {
